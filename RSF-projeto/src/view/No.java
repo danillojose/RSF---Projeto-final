fecd68ff6a0f48f819304de8ae322eba016e5378
andrews371 <40750626+andrews371@users.noreply.github.com>
2018-10-30 13:54:57 -0300

    Update No.java

--- RSF-projeto/src/view/No.java
+++ RSF-projeto/src/view/No.java
@@ -1,5 +1,20 @@
 package view;
 
 public class No {
+	private int id, alcance = 1;
+	
+	public void setAlcance(int alcance) {
+		this.alcance = alcance;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getAlcance()
+		return alcance;
+	
+	public int getId()
+		return id;
 	
 }
